a25d061f5febdbff85b61675068692e9c487bc3b
Istebrek <istebrek.akram@outlook.com>
2025-12-26 14:20:57 +0100

    Solve Part 2 with 3 separate attempts and 2 different approaches.

--- Day1/PasswordHunt/Program.cs
+++ Day1/PasswordHunt/Program.cs
@@ -60,51 +60,104 @@ foreach (char c in input)
 // Console.WriteLine(instructions.Count());
 
 //Step 4: Iterate through the list of instructions and turn the dial.
-int start = 50;
+int dial = 50;
 List<int> landsOn = new();
 
-foreach (string s in instructions)
-{
-    if (s.Contains('R'))
-    {
-        string n = s.Substring(1);
-        int num = Convert.ToInt32(n);
-        int index = start+num;
-        if (index <= 99)
-        {
-            start = index;
-            landsOn.Add(start);
-        }
-        if (index > 99)
-        {
-            while (index > 99)
-            {
-                start = index -= 100;
-            }   
-            landsOn.Add(start);
-        }
+// foreach (string s in instructions)
+// {
+//     if (s.Contains('R'))
+//     {
+//         string n = s.Substring(1);
+//         int num = Convert.ToInt32(n);
+//         int index = dial+num;
+//         if (index <= 99)
+//         {
+//             dial = index;
+//             landsOn.Add(dial);
+//         }
+//         if (index > 99)
+//         {
+//             while (index > 99)
+//             {
+//                 dial = index -= 100;
+//             }   
+//             landsOn.Add(dial);
+//         }
 
-    }
-    if (s.Contains('L'))
-    {
-        string n = s.Substring(1);
-        int num = Convert.ToInt32(n);
-        int index = start-num;
-        if (index >= 0)
-        {
-            start = index;
-            landsOn.Add(start);
-        }
-        if (index < 0) 
-        {
-            while (index < 0)
-            {
-                start = index += 100;
-            }
-            landsOn.Add(start);
-        }
-    }
-}
+//     }
+//     if (s.Contains('L'))
+//     {
+//         string n = s.Substring(1);
+//         int num = Convert.ToInt32(n);
+//         int index = dial-num;
+//         if (index >= 0)
+//         {
+//             dial = index;
+//             landsOn.Add(dial);
+//         }
+//         if (index < 0) 
+//         {
+//             while (index < 0)
+//             {
+//                 dial = index += 100;
+//             }
+//             landsOn.Add(dial);
+//         }
+//     }
+// }
+
+
+/* PART 2 */
+// int zero = 0;
+// foreach (string s in instructions)
+// {
+//     if (s.Contains('R'))
+//     {
+//         string n = s.Substring(1);
+//         int num = Convert.ToInt32(n);
+//         int index = dial+num;
+//         if (index == 0) zero++;
+//         if (index <= 99)
+//         {
+//             dial = index;
+//             landsOn.Add(dial);
+//         }
+//         while (index > 99)
+//         {
+//             dial = index -= 100;
+//             landsOn.Add(dial);
+//             zero++;
+//         }
+//     }
+//     if (s.Contains('L'))
+//     {
+//         string n = s.Substring(1);
+//         int num = Convert.ToInt32(n);
+//         int index = dial-num;
+
+//         if (index == 0) zero++;            
+//         if (index >= 0)
+//         {
+//             dial = index;
+//             landsOn.Add(dial);
+//         }
+//         while (index < 0)
+//         {
+//             dial = index += 100;
+//             landsOn.Add(dial);
+//             zero++;
+//         }
+//     }
+// }
+// Console.WriteLine($"{zero}");
+// int j = 0;
+
+// Console.WriteLine("- The dial starts by pointing at 50.");
+// foreach (int rotation in landsOn)
+// {
+//     Console.WriteLine($"- The dial is rotated {instructions[j]} to point at {rotation}");
+//     j++;
+// }
 
 //Ensure you get correct value, then comment it out. Below index expects value 28.
 // Console.WriteLine(landsOn[6]);
@@ -115,17 +168,109 @@ foreach (string s in instructions)
 // }
 
 //Ensure all rotations are accounted for, then comment it out. Below count should be 4177.
-Console.WriteLine(landsOn.Count());
+// Console.WriteLine(landsOn.Count());
 
 //Step 5: Sum the times the dial lands on 0.
-int zeroes = 0;
-foreach (int n in landsOn)
+// int zeroes = 0;
+// foreach (int n in landsOn)
+// {
+//     if (n == 0)
+//     {
+//         zeroes++;
+//     }
+// }
+
+//Test against submission at the official site, should be 982.
+// Console.WriteLine(zeroes);
+
+
+//PART 2:
+// int zero = 0;
+// int k = 0;
+// Console.WriteLine($"Dial starts by pointing at {dial}.");
+
+// foreach (string rotation in instructions)
+// {
+//     k = 0;
+//     if (rotation.Contains('R'))
+//     {
+//         int rotationNumber = Convert.ToInt32(rotation.Substring(1));
+//         int sum = dial + rotationNumber;
+
+//         if (sum <= 99) 
+//         {
+//             dial = sum;
+//             landsOn.Add(dial);
+//             Console.WriteLine($"Dial is rotated {rotation} and points at {dial}.");
+//         }
+//         if (sum > 99)
+//         {
+//             while (sum > 99)
+//             {
+//                 dial = sum -= 100;
+//                 landsOn.Add(dial);
+//                 zero++;
+//                 k++;
+//             }            
+//             Console.WriteLine($"Dial is rotated {rotation} and points at {dial}; it points at 0 {k} times.");
+//         }
+//     }
+
+//     if (rotation.Contains('L'))
+//     {
+//         // Console.WriteLine($"Dial points at {dial}.");
+
+//         int rotationNumber = Convert.ToInt32(rotation.Substring(1));
+//         int sum = dial - rotationNumber;
+
+//         if (sum >= 0) 
+//         {
+//             dial = sum;
+//             landsOn.Add(dial);
+//             Console.WriteLine($"Dial is rotated {rotation} and points at {dial}.");
+//         }
+//         if (sum < 0 && dial != 0)
+//         {
+//             while (sum < 0)
+//             {
+//                 dial = sum += 100;
+//                 landsOn.Add(dial);
+//                 zero++;
+//                 k++;
+//             }
+//             Console.WriteLine($"Dial is rotated {rotation} and points at {dial}; it points at 0 {k} times.");
+//         }
+//         if (dial == 0)
+//         {
+//             dial = sum +=100;
+//             landsOn.Add(dial);
+//         }
+//     }
+// }
+
+// Console.WriteLine(zero);
+
+//PART 2 attempt 3
+int answer = 0;
+
+foreach (string rotation in instructions)
 {
-    if (n == 0)
+    int turns = Convert.ToInt32(rotation.Substring(1));
+    for (int i = 0; i < turns; i++)
     {
-        zeroes++;
+        if (rotation.Contains('R'))
+        {
+            dial -= 1;
+        }
+        if (rotation.Contains('L'))
+        {
+            dial += 1;
+        }
+        if ((dial % 100) == 0)
+        {
+            answer++;
+        }
     }
 }
 
-//Test against submission at the official site, should be 982.
-Console.WriteLine(zeroes);
+Console.WriteLine(answer);
