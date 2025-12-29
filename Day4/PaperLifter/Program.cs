6888580308c27bc24b8d11f44fea7af66295d82e
Istebrek <istebrek.akram@outlook.com>
2025-12-29 23:45:34 +0100

    WIP implement conditional loops. Fix needed for corners and edges.

--- Day4/PaperLifter/Program.cs
+++ Day4/PaperLifter/Program.cs
@@ -1,4 +1,58 @@
 ﻿string path = AppContext.BaseDirectory;
 string allRolls = File.ReadAllText(path + "../../../example.txt");
 
-Console.WriteLine(allRolls);
+List<string> rolls = allRolls.Split('\n').Select(rolls => rolls.Trim()).ToList();
+int positionsPerRow = rolls[0].Length;
+
+allRolls = "";
+foreach (string roll in rolls) allRolls += roll;
+
+int accessSum = 0;
+int rounds = 0;
+for (int i = 0; i < allRolls.Length; i++)
+{
+    char c = allRolls[i];
+    int adjacents = 0;
+    rounds++;
+
+    if (c == '@')
+    {
+        if (i > positionsPerRow)
+        {
+            if (allRolls[i - positionsPerRow] == '@') adjacents++;
+        }
+        if (i != rounds * positionsPerRow)
+        {
+            if (allRolls[i - 1] == '@' && i > 0) adjacents++;
+            if (i < positionsPerRow)
+            {
+                if (allRolls[i + positionsPerRow - 1] == '@') adjacents++;                
+            }
+            if (i > positionsPerRow)
+            {
+                if (allRolls[i - positionsPerRow - 1] == '@') adjacents++;
+            }
+        }
+        if (i != rounds * positionsPerRow - 1)
+        {
+            if (allRolls[i + 1] == '@' && i < allRolls.Length) adjacents++;
+            if (i < positionsPerRow)
+            {
+                if (allRolls[i + positionsPerRow + 1] == '@') adjacents++;
+            }
+            if (i > positionsPerRow)
+            {
+                if (allRolls[i - positionsPerRow + 1] == '@') adjacents++;
+            }
+        }
+        if (i < positionsPerRow)
+        {
+            if (allRolls[i + positionsPerRow] == '@') adjacents++;
+        }
+        if (adjacents < 4) accessSum++;
+    }
+}
+// Console.WriteLine(allRolls);
+// Console.WriteLine(allRolls.Length);
+// Console.WriteLine(positionsPerRow);
+Console.WriteLine(accessSum);
