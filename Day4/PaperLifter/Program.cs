1aeca936c4f68c563a35abbe5d6651ee69d4ba47
Istebrek <istebrek.akram@outlook.com>
2025-12-30 23:44:45 +0100

    Solve for Part 1 where each @ that has less than 4 '@' around it are summed up.

--- Day4/PaperLifter/Program.cs
+++ Day4/PaperLifter/Program.cs
@@ -1,53 +1,99 @@
-﻿string path = AppContext.BaseDirectory;
-string allRolls = File.ReadAllText(path + "../../../example.txt");
+﻿using PaperLifter;
+
+string path = AppContext.BaseDirectory;
+string allRolls = File.ReadAllText(path + "../../../input.txt");
 
 List<string> rolls = allRolls.Split('\n').Select(rolls => rolls.Trim()).ToList();
-int positionsPerRow = rolls[0].Length;
+int row = rolls[0].Length;
+int totalRows = rolls.Count();
 
 allRolls = "";
 foreach (string roll in rolls) allRolls += roll;
 
 int accessSum = 0;
 int rounds = 0;
+int fullRound = 0;
+Positions positions = new();
 for (int i = 0; i < allRolls.Length; i++)
 {
     char c = allRolls[i];
     int adjacents = 0;
+    if (rounds == row) 
+    {
+        fullRound++;
+        rounds = 0;
+    }
     rounds++;
 
     if (c == '@')
     {
-        if (i > positionsPerRow)
+        if (i == fullRound * row) //if character is at a position on the left.
         {
-            if (allRolls[i - positionsPerRow] == '@') adjacents++;
-        }
-        if (i != rounds * positionsPerRow)
+            if (i < row) //if character is on the top row.
+            {
+                if (positions.Down(i, row, allRolls) == '@') adjacents++;
+                if (positions.RightDown(i, row, allRolls) == '@') adjacents++;
+                if (positions.Right(i, allRolls) == '@') adjacents++;
+            } else if (i >= (totalRows * row) - row) //if character is on the bottom row.
+            {
+                if (positions.Up(i, row, allRolls) == '@') adjacents++;
+                if (positions.RightUp(i, row, allRolls) == '@') adjacents++;
+                if (positions.Right(i, allRolls) == '@') adjacents++;
+            } else
+            {
+                if (positions.Down(i, row, allRolls) == '@') adjacents++;
+                if (positions.RightDown(i, row, allRolls) == '@') adjacents++;
+                if (positions.Right(i, allRolls) == '@') adjacents++;
+                if (positions.Up(i, row, allRolls) == '@') adjacents++;
+                if (positions.RightUp(i, row, allRolls) == '@') adjacents++;
+            }
+        } else if (i == (row * (fullRound + 1)) - 1) //if character is at the right-most position.
         {
-            if (allRolls[i - 1] == '@' && i > 0) adjacents++;
-            if (i < positionsPerRow)
+            if (i < row) //if character is on the top row.
             {
-                if (allRolls[i + positionsPerRow - 1] == '@') adjacents++;                
-            }
-            if (i > positionsPerRow)
+                if (positions.Down(i, row, allRolls) == '@') adjacents++;
+                if (positions.LeftDown(i, row, allRolls) == '@') adjacents++;
+                if (positions.Left(i, allRolls) == '@') adjacents++;
+            } else if (i >= (totalRows * row) - row) //if character is on the bottom row.
             {
-                if (allRolls[i - positionsPerRow - 1] == '@') adjacents++;
+                if (positions.Up(i, row, allRolls) == '@') adjacents++;
+                if (positions.LeftUp(i, row, allRolls) == '@') adjacents++;
+                if (positions.Left(i, allRolls) == '@') adjacents++;
+            } else
+            {
+                if (positions.Down(i, row, allRolls) == '@') adjacents++;
+                if (positions.LeftDown(i, row, allRolls) == '@') adjacents++;
+                if (positions.Left(i, allRolls) == '@') adjacents++;
+                if (positions.Up(i, row, allRolls) == '@') adjacents++;
+                if (positions.LeftUp(i, row, allRolls) == '@') adjacents++;
             }
-        }
-        if (i != rounds * positionsPerRow - 1)
+        } else //if character is not at the right-most or left-most position.
         {
-            if (allRolls[i + 1] == '@' && i < allRolls.Length) adjacents++;
-            if (i < positionsPerRow)
+            if (i < row) //if character is on the top row.
             {
-                if (allRolls[i + positionsPerRow + 1] == '@') adjacents++;
-            }
-            if (i > positionsPerRow)
+                if (positions.Down(i, row, allRolls) == '@') adjacents++;
+                if (positions.LeftDown(i, row, allRolls) == '@') adjacents++;
+                if (positions.RightDown(i, row, allRolls) == '@') adjacents++;
+                if (positions.Left(i, allRolls) == '@') adjacents++;
+                if (positions.Right(i, allRolls) == '@') adjacents++;
+            } else if (i >= (totalRows * row) - row) //if character is on the bottom row.
             {
-                if (allRolls[i - positionsPerRow + 1] == '@') adjacents++;
+                if (positions.Up(i, row, allRolls) == '@') adjacents++;
+                if (positions.LeftUp(i, row, allRolls) == '@') adjacents++;
+                if (positions.RightUp(i, row, allRolls) == '@') adjacents++;
+                if (positions.Left(i, allRolls) == '@') adjacents++;
+                if (positions.Right(i, allRolls) == '@') adjacents++;
+            } else // a middle position
+            {
+                if (positions.Down(i, row, allRolls) == '@') adjacents++;
+                if (positions.LeftDown(i, row, allRolls) == '@') adjacents++;
+                if (positions.RightDown(i, row, allRolls) == '@') adjacents++;
+                if (positions.Left(i, allRolls) == '@') adjacents++;
+                if (positions.Right(i, allRolls) == '@') adjacents++;
+                if (positions.Up(i, row, allRolls) == '@') adjacents++;
+                if (positions.LeftUp(i, row, allRolls) == '@') adjacents++;
+                if (positions.RightUp(i, row, allRolls) == '@') adjacents++;
             }
-        }
-        if (i < positionsPerRow)
-        {
-            if (allRolls[i + positionsPerRow] == '@') adjacents++;
         }
         if (adjacents < 4) accessSum++;
     }
