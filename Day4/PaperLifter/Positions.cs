b343173dc7b61d4357b6823efeffbfac76c0196d
Istebrek <istebrek.akram@outlook.com>
2025-12-31 00:46:13 +0100

    Add class to handle character positions

---
+++ Day4/PaperLifter/Positions.cs
@@ -0,0 +1,37 @@
+﻿namespace PaperLifter;
+
+public class Positions
+{
+    public char LeftUp(int i, int row, string allRolls)
+    {
+        return allRolls[i - row - 1];        
+    }
+    public char Up(int i, int row, string allRolls)
+    {
+        return allRolls[i - row];        
+    }
+    public char RightUp(int i, int row, string allRolls)
+    {
+        return allRolls[i - row + 1];    
+    }
+    public char Left(int i, string allRolls)
+    {
+        return allRolls[i - 1];        
+    }
+    public char Right(int i, string allRolls)
+    {
+        return allRolls[i + 1];        
+    }
+    public char LeftDown(int i, int row, string allRolls)
+    {
+        return allRolls[i + row - 1];        
+    }
+    public char Down(int i, int row, string allRolls)
+    {
+        return allRolls[i + row];        
+    }
+    public char RightDown(int i, int row, string allRolls)
+    {
+        return allRolls[i + row + 1];        
+    }
+}
