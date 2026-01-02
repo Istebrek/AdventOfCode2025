4c5713a4b0410d7acde89b6418190c73f6077805
Istebrek <istebrek.akram@outlook.com>
2026-01-02 19:56:43 +0100

    Solve for part 1 without OK memory

--- Day5/FreshOnes/Program.cs
+++ Day5/FreshOnes/Program.cs
@@ -8,12 +8,30 @@ string entireInput = inputs.Input(input);
 
 List<string> fullInput = entireExample.Split('\n').Select(x => x.Trim()).ToList();
 // List<string> fullInput = entireInput.Split('\n').Select(x => x.Trim()).ToList();
+int indexFrom = fullInput.FindIndex(x => x == "") + 1;
 
-Separated separated = new();
-List<string> allRanges = separated.RangeList(fullInput);
-List<string> allIds = separated.IdList(fullInput);
+int freshCount = 0;
+for (int i = indexFrom; i < fullInput.Count; i++)
+{
+    string id = fullInput[i];
 
-FreshOnesAnswer freshSum = new();
-long answer = freshSum.TotalFreshIngredients(allRanges, allIds);
+    if (string.IsNullOrWhiteSpace(id)) continue;
 
-Console.WriteLine(answer);
+    for (int j = 0; j < indexFrom - 1; j++)
+    {
+        string range = fullInput[j];
+        List<string> ranges = range.Split("-").ToList();
+        long start = Convert.ToInt64(ranges[0]);
+        long end = Convert.ToInt64(ranges[1]);
+
+        if (Convert.ToInt64(id) < start || Convert.ToInt64(id) > end) continue;
+        if (Convert.ToInt64(id) >= start && Convert.ToInt64(id) <= end)
+        {
+            freshCount++;
+            break;
+        }
+
+    }
+}
+
+Console.WriteLine(freshCount);
