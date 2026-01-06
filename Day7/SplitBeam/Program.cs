7f1e92aa1254545c77ca7bb49f9dbc55ee271133
Istebrek <istebrek.akram@outlook.com>
2026-01-06 23:55:21 +0100

    Solve for example, input needs work.

--- Day7/SplitBeam/Program.cs
+++ Day7/SplitBeam/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿string splitters = File.ReadAllText(AppContext.BaseDirectory + "../../../input.txt");
+List<string> rows = splitters.Split('\n').Select(x => x.Trim()).ToList();
+int rowCount = rows.Count;
+
+int splits = 0;
+List<int> start = new();
+start.Add(splitters.IndexOf('S'));
+bool isSplit = false;
+for (int i = 1; i < rows.Count; i++)
+{
+    isSplit = false;
+    string row = rows[i];
+    int index = 0;
+    if (row.Contains('^'))
+    {
+        for (int c = 0; c < row.Length; c++)
+        {
+            if (row[c] != '^') continue;
+            index = c;
+            if (start.Contains(index))
+            {
+                if (!start.Contains(index + 1) && index < row.Length) 
+                {
+                    start.Add(index + 1);
+                    start.Remove(index);
+                    isSplit = true;
+                }
+                if (!start.Contains(index - 1) && index > 0) 
+                {
+                    start.Add(index - 1);
+                    start.Remove(index);
+                    isSplit = true;
+                }
+                if (isSplit == true) splits++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(splits);
