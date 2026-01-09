173d0651e5707f5c325a0edee1104970b67d0d88
Istebrek <istebrek.akram@outlook.com>
2026-01-09 19:48:50 +0100

    Update logic for substrings to correctly change input.

--- Day7/SplitBeam/Timelines.cs
+++ Day7/SplitBeam/Timelines.cs
@@ -5,30 +5,34 @@ public class Timelines
     public int TimelineCount(List<string> rows, string input)
     {
         int timelines = 0;
-        int startIndex = rows[0].IndexOf('S');
         bool newPath = false;
+        bool right = true; 
 
         do
         {
+            int startIndex = rows[0].IndexOf('S');
             newPath = false;
             for (int i = 1; i < rows.Count; i++)
             {
                 string row = rows[i];
                 int rowL = row.Length;
-                if (row.IndexOf('^') == startIndex)
+                if (row[startIndex].Equals('.')) input = input.Substring(0, startIndex + i * rowL) + '|' + input.Substring(startIndex + i * rowL + 1);
+                if (row[startIndex].Equals('^'))
                 {
-                    input = input.Substring(0, startIndex + (i-1) * rowL) + '|' + input.Substring(startIndex + (i-1) * rowL + 1);
-                    if (startIndex >= 0 && startIndex < rowL && !row[startIndex + 1].Equals('|'))
+                    if (right == true && startIndex >= 0 && startIndex < rowL)
                     {
+                        if (!input[startIndex+1 + (i+1) * rowL].Equals('|')) newPath = true;
                         input = input.Substring(0, startIndex + 1 + i * rowL) + '|' + input.Substring(startIndex + 2 + i * rowL);
-                        newPath = true;
-                    } else if (startIndex != 0 && startIndex <= rowL && !row[startIndex - 1].Equals('|'))
+                        startIndex += 1;
+                    } else if (right == false && startIndex > 0 && startIndex <= rowL)
                     {
-                        row = input.Substring(0, startIndex - 1 + i * rowL) + '|' + input.Substring(startIndex - 2 + i * rowL);
-                        newPath = true;
+                        if (!input[startIndex-1 + (i+1) * rowL].Equals('|')) newPath = true;
+                        input = input.Substring(0, startIndex - 1 + i * rowL) + '|' + input.Substring(startIndex + i * rowL);
+                        startIndex -= 1;
                     } 
                 }
             }
+            right = (right == true) ? false : true;
             if (newPath == true) timelines++;
         } while (newPath == true);
 
